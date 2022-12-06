454c2669c5b48eee33bf7a9433c82ac592ad1d39
Akshay More <45824962+theakshaymore@users.noreply.github.com>
2022-12-06 19:09:55 +0530

    try2

--- projfrontend/src/core/helper/coreapicalls.js
+++ projfrontend/src/core/helper/coreapicalls.js
@@ -1,4 +1,6 @@
-import { API } from "../../backend";
+// import { API } from "../../backend";
+
+const API = "https://embrand.vercel.app/api/";
 
 export const getProducts = () => {
   return fetch(`${API}/products`, { method: "GET" })
